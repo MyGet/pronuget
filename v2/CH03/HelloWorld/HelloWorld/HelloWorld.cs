2a9c0c3f2304ecb997fd0435dca5c57499120aae
Xavier Decoster <xavier.decoster@gmail.com>
2013-07-17 21:04:24 +0200

    Added HelloWorld solution

---
+++ v2/CH03/HelloWorld/HelloWorld/HelloWorld.cs
@@ -0,0 +1,10 @@
+﻿namespace HelloWorld
+{
+    public class HelloWorld : IFluentInterface
+    {
+        public string Greet(string friendlyPerson)
+        {
+            return string.Format("Hello world, {0}!", friendlyPerson);
+        }
+    }
+}
---
+++ v2/CH03/HelloWorld/HelloWorld/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HelloWorld")]
+[assembly: AssemblyDescription("A HelloWorld class, written in C#.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Pro NuGet")]
+[assembly: AssemblyProduct("HelloWorld")]
+[assembly: AssemblyCopyright("Copyright © Maarten Balliauw and Xavier Decoster 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("457ab5f2-3473-4306-bd78-119ad3b7d92a")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
