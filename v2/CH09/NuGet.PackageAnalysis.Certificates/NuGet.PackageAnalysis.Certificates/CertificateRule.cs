16ec9cb1ca2ee95c43f7359da7d11ce1be5e4d70
Maarten Balliauw <maarten@balliauw.be>
2013-07-26 14:12:52 +0200

    Added code for NuGet.PackageAnalysis.Certificates
    Added code for NuGet.PackageAnalysis.Certificates

---
+++ v2/CH09/NuGet.PackageAnalysis.Certificates/NuGet.PackageAnalysis.Certificates/CertificateRule.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.Composition;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace NuGet.PackageAnalysis.Certificates
+{
+    [Export(typeof(IPackageRule))]
+    public sealed class CertificateRule : IPackageRule
+    {
+        private readonly string[] _forbiddenExtensions = { ".cer", ".pfx" };
+
+        public IEnumerable<PackageIssue> Validate(IPackage package)
+        {
+            // Loop through all files and match them with _forbiddenExtensions
+            foreach (var packageFile in package.GetFiles())
+            {
+                if (_forbiddenExtensions.Any(
+                    extension => extension == Path.GetExtension(packageFile.Path).ToLowerInvariant()))
+                {
+                    yield return new PackageIssue(
+                        "Forbidden file extension found", 
+                        string.Format("The file {0} is of a forbidden file extension and may not be included in a NuGet package.", packageFile.Path), 
+                        "Remove it from the package folder.", 
+                        PackageIssueLevel.Error);
+                }
+            }
+        }
+    }
+}
